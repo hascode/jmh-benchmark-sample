48fa9085203be5d834bc49d128efcae5a2c34867
Micha Kops <micha.kops@gmail.com>
2017-10-02 18:33:21 +0200

    Programmatic configuration example fixed.

--- src/main/java/com/hascode/tutorial/ProgrammaticalConfigExample.java
+++ src/main/java/com/hascode/tutorial/ProgrammaticalConfigExample.java
@@ -1,5 +1,7 @@
 package com.hascode.tutorial;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -12,11 +14,17 @@ public class ProgrammaticalConfigExample {
         .include(".*")
         .warmupIterations(10)
         .measurementIterations(20)
-        .jvmArgs("-Xms2g", "-Xmx2g")
+        .jvmArgs("-Xms4g", "-Xmx4g")
         .shouldDoGC(true)
         .forks(1)
+        .mode(Mode.AverageTime)
         .build();
 
     new Runner(opts).run();
+  }
+
+  @Benchmark
+  public void sample(){
+
   }
 }
